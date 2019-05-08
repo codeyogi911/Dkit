1154a8b41aca3683fdb6567e5ff8f7c585d71a6b
Shashwat Jain <shashwatjain@icloud.com>
2019-05-08 18:14:42 +0800

    Working Create Trainer Script

--- src/create_trainer.sh
+++ src/create_trainer.sh
@@ -1,8 +1,7 @@
 # Create new ec2 machine
-docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 150 --amazonec2-instance-type p2.xlarge dkit-trainer
+docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 150 --amazonec2-instance-type p2.xlarge dkit-trainer && \
 # Setup Dkit in the system
-docker-machine ssh dkit-trainer source activate tensorflow_p36
-docker-machine ssh dkit-trainer git clone https://github.com/codeyogi911/Dkit.git
-docker-machine ssh dkit-trainer python Dkit/setup.py install
-# Setup dataset
-docker-machine ssh dkit-trainer aws s3 sync s3://dkitdatasets /home/ubuntu/Dkit/datasets/
+docker-machine ssh dkit-trainer << EOF
+source activate tensorflow_p36 && git clone https://github.com/codeyogi911/Dkit.git && \
+cd Dkit && python setup.py install && aws s3 sync s3://dkitdatasets /home/ubuntu/Dkit/datasets/
+EOF
