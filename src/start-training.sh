4b05ceb5d58bd4b607882564d837525d96774e79
Shashwat Jain <shashwatjain@icloud.com>
2019-05-09 18:51:58 +0800

    Added tensorboard support

--- src/start-training.sh
+++ src/start-training.sh
@@ -2,11 +2,13 @@ aws s3 sync ../datasets s3://dkitdatasets && \
 docker-machine start dkit-trainer
 docker-machine ssh dkit-trainer -L localhost:6006:dkit-trainer:6006 << EOF
 cd Dkit && \
+rm -r datasets/ && \
 aws s3 sync s3://dkitdatasets datasets/ && \
 source activate tensorflow_p36 && \
 tensorboard --logdir logs/ & \
-cd src && python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco && \
-cd .. && aws s3 sync logs/ s3://dkitmodels-sg 
+source activate tensorflow_p36 && \
+cd Dkit/src && python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=imagenet && \
+cd ../logs && cd $(ls -td -- */ | head -n 1) && aws s3 sync mask_rcnn_dkit_0030.h5 s3://dkitmodels-sg/mask_rcnn_dkit_latest.h5 
 EOF
-docker-machine stop dkit-trainer && \
-aws s3 sync s3://dkitmodels-sg ../logs/
+docker-machine stop dkit-trainer
+aws s3 sync s3://dkitmodels-sg ../models
