5b8a58182c01a9578a8d73b54247ad82346a8739
Shashwat Jain <shashwatjain@icloud.com>
2019-05-09 14:14:39 +0800

    Scripts - alpha

--- src/create_trainer.sh
+++ src/create_trainer.sh
@@ -1,7 +1,8 @@
+aws s3 sync ../datasets s3://dkitdatasets && \
 # Create new ec2 machine
 docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 150 --amazonec2-instance-type p2.xlarge dkit-trainer && \
 # Setup Dkit in the system
 docker-machine ssh dkit-trainer << EOF
 source activate tensorflow_p36 && git clone https://github.com/codeyogi911/Dkit.git && \
-cd Dkit && python setup.py install && aws s3 sync s3://dkitdatasets /home/ubuntu/Dkit/datasets/
+cd Dkit && python setup.py install && aws s3 sync s3://dkitdatasets datasets/
 EOF
--- src/start-training.sh
+++ src/start-training.sh
@@ -1,5 +1,10 @@
-aws s3 sync s3://dkitdatasets /home/ubuntu/Dkit/datasets/
-source activate tensorflow_p36
+aws s3 sync ../datasets s3://dkitdatasets && \
+docker-machine start dkit-trainer
 docker-machine ssh dkit-trainer << EOF
-cd Dkit/src/ && python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco
+cd Dkit && \
+aws s3 sync s3://dkitdatasets datasets/ && \
+source activate tensorflow_p36 && \
+cd Dkit/src/ && python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco && \
+cd .. && aws s3 sync logs/ s3://dkitmodels 
 EOF
+docker-machine stop dkit-trainer
