6da262459368fa3c54a7cf0a37c8ef7c00c6f9dd
Shashwat Jain <shashwatjain@icloud.com>
2019-05-09 16:56:43 +0800

    Changed logs bucket

--- src/start-training.sh
+++ src/start-training.sh
@@ -1,10 +1,12 @@
 aws s3 sync ../datasets s3://dkitdatasets && \
 docker-machine start dkit-trainer
-docker-machine ssh dkit-trainer << EOF
+docker-machine ssh dkit-trainer -L localhost:6006:dkit-trainer:6006 << EOF
 cd Dkit && \
 aws s3 sync s3://dkitdatasets datasets/ && \
 source activate tensorflow_p36 && \
-cd Dkit/src/ && python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco && \
-cd .. && aws s3 sync logs/ s3://dkitmodels 
+tensorboard --logdir logs/ & \
+cd src && python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco && \
+cd .. && aws s3 sync logs/ s3://dkitmodels-sg 
 EOF
-docker-machine stop dkit-trainer
+docker-machine stop dkit-trainer && \
+aws s3 sync s3://dkitmodels-sg ../logs/
