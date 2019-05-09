ba831a6a2d5e1e54f38b5b1fc75ea04196a1deba
Shashwat Jain <shashwatjain@icloud.com>
2019-05-09 09:37:10 +0800

    Changed start trainning script

--- src/start-training.sh
+++ src/start-training.sh
@@ -1,4 +1,4 @@
-# aws s3 sync s3://dkitdatasets /home/ubuntu/Dkit/datasets/
+aws s3 sync s3://dkitdatasets /home/ubuntu/Dkit/datasets/
 source activate tensorflow_p36
 docker-machine ssh dkit-trainer << EOF
 cd Dkit/src/ && python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco
