542b3e55010c5c164626e51a02a70ccdec0f9553
Shashwat Jain <shashwatjain@icloud.com>
2019-05-10 09:24:43 +0800

    updated to p3.2xlarge

--- src/create_trainer.sh
+++ src/create_trainer.sh
@@ -1,6 +1,12 @@
-aws s3 sync ../datasets s3://dkitdatasets && \
+if [ -d "../datasets" ]
+then
+    aws s3 sync ../datasets s3://dkitdatasets && \
+else
+    aws s3 sync s3://dkitdatasets ../datasets && \
+fi
+# aws s3 sync ../datasets s3://dkitdatasets && \
 # Create new ec2 machine
-docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 150 --amazonec2-instance-type p2.xlarge dkit-trainer && \
+docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 150 --amazonec2-instance-type p3.2xlarge dkit-trainer && \
 # Setup Dkit in the system
 docker-machine ssh dkit-trainer << EOF
 source activate tensorflow_p36 && git clone https://github.com/codeyogi911/Dkit.git && \
