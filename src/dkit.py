e6a44a42d0757b58ec05c1d81467ce895bbf7f97
Shashwat Jain <shashwatjain@icloud.com>
2019-05-13 16:06:29 +0800

    Fixed auto model copy

--- src/dkit.py
+++ src/dkit.py
@@ -69,7 +69,7 @@ class DkitConfig(Config):
     IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 6  # Background + surgical items
+    NUM_CLASSES = 1 + 7  # Background + surgical items
 
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
@@ -96,6 +96,7 @@ class DkitDataset(utils.Dataset):
         self.add_class("dkit", 4, "Bit 1")
         self.add_class("dkit", 5, "Bit 8")
         self.add_class("dkit", 6, "Bit 2")
+        self.add_class("dkit", 7, "Bit 6")
         # Train or validation dataset?
         assert subset in ["train", "val"]
         dataset_dir = os.path.join(dataset_dir, subset)
--- src/start-training.sh
+++ src/start-training.sh
@@ -2,12 +2,13 @@ aws s3 sync ../datasets s3://dkitdatasets && \
 docker-machine start dkit-trainer
 docker-machine ssh dkit-trainer << EOF
 cd Dkit && \
-rm -r datasets/ && \
+git pull && \
 aws s3 sync s3://dkitdatasets datasets/ && \
 source activate tensorflow_p36 && \
-cd Dkit/src && python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco
-cd .. && cd logs && cd $(ls -td -- */ | head -n 1) && \
-aws s3 cp mask_rcnn_dkit_0030.h5 s3://dkitmodels-sg/mask_rcnn_dkit_coco_latest.h5
+cd src && \
+python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco && \
+var=\$(find ../logs -type f -name "mask_rcnn_dkit_0030.h5" | sort -r | head -n 1) && \
+aws s3 cp "\$var" s3://dkitmodels-sg/mask_rcnn_dkit_coco_latest.h5
 exit
 EOF
 docker-machine stop dkit-trainer
