cc89ae8df442c42de1be40622ff2728936bfceec
Shashwat Jain <shashwatjain@icloud.com>
2019-05-15 13:06:09 +0800

    Removed mini masks

--- src/dkit.py
+++ src/dkit.py
@@ -77,6 +77,7 @@ class DkitConfig(Config):
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
+    USE_MINI_MASK = False
 
 
 ############################################################
--- src/serve-model-aws.sh
+++ src/serve-model-aws.sh
@@ -1,6 +1,10 @@
 docker-machine ssh dkit-trainer -L localhost:5000:dkit-trainer:5000 << EOF
-cd Dkit && \
+cd Dkit/src && \
 source activate tensorflow_p36 && \
 aws s3 sync s3://dkitmodels-sg ../models && \
 python3 serve.py
 EOF
+# cd /tmp/tfserving
+# docker run --runtime=nvidia -p 8501:8501 --mount type=bind,\
+# source=/tmp/tfserving/serving/tensorflow_serving/servables/tensorflow/testdata/saved_model_half_plus_two_gpu,\
+# target=/models/half_plus_two -e MODEL_NAME=half_plus_two -t tensorflow/serving:latest-gpu
