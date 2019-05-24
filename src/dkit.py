54e8473f19334beead8c2e40c6d12d0dbc9331e4
Shashwat Jain <shashwatjain@Shashwats-MacBook-Pro.local>
2019-05-24 10:13:45 +0800

    Increase epochs

--- src/dkit.py
+++ src/dkit.py
@@ -215,7 +215,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
+                epochs=40,
                 layers='heads',
                 augmentation = imgaug.augmenters.Sometimes(0.5, [
                     imgaug.augmenters.Fliplr(0.5),
