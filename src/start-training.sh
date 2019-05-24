b35db213d8c9f679f235231fca966e885d42244a
Shashwat Jain <shashwatjain@Shashwats-MacBook-Pro.local>
2019-05-24 10:59:03 +0800

    changes to training script

--- src/start-training.sh
+++ src/start-training.sh
@@ -7,9 +7,9 @@ git pull && \
 aws s3 sync s3://dkitdatasets datasets/ && \
 source activate tensorflow_p36 && \
 cd src && \
-python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=coco
-var=\$(find ../logs -type f -name "mask_rcnn_dkit_0030.h5" | sort -r | head -n 1) && \
-aws s3 cp "\$var" s3://dkitmodels-sg/mask_rcnn_dkit_coco_latest.h5 && \
+python3 dkit.py train --dataset=../datasets/IKEA_FIXA --weights=last
+var=\$(find ../logs -type f -name "mask_rcnn_dkit_0030.h5" | sort -r | head -n 1)
+aws s3 cp "\$var" s3://dkitmodels-sg/mask_rcnn_dkit_coco_latest.h5
 exit
 EOF
 docker-machine stop dkit-trainer
