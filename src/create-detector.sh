26483f06ed87fabe8145a926f3df343f75bc288f
Shashwat Jain <shashwatjain@Shashwats-MacBook-Pro.local>
2019-09-03 13:06:06 +0800

    added sagemaker notebook

--- src/create-detector.sh
+++ src/create-detector.sh
@@ -6,7 +6,7 @@ else
 fi
 # aws s3 sync ../datasets s3://dkitdatasets && \
 # Create new ec2 machine
-docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 150 --amazonec2-instance-type p3.2xlarge dkit-detector
+docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 150 --amazonec2-instance-type p2.xlarge dkit-detector
 # Setup Dkit in the system
 docker-machine ssh dkit-detector << EOF
 source activate tensorflow_p36 && pip install git+https://github.com/aleju/imgaug.git && \
---
+++ utilities/create_GT_manifest.py
@@ -0,0 +1,14 @@
+import glob
+import json
+import os
+IMAGE_PATHS = glob.glob("datasets/IKEA_FIXA/scaled/*.jpeg")
+IMAGE_PATHS.extend(glob.glob("datasets/IKEA_FIXA/scaled/*.jpg"))
+for path in IMAGE_PATHS:
+    data = {}
+    data['source-ref'] = "s3://bit-groundtruth/images/" + \
+        os.path.basename(path)
+    # print("Trying {}".format(path))
+    # print(os.path.basename(path))
+    with open('utilities/manifest.json', 'a') as outfile:
+        outfile.write(json.dumps(data)+"\n")
+        # json.dump(data, outfile)
