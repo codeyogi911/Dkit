94a58711771ddc5587bbb445cc9a0cd28965ae9a
Shashwat Jain <shashwatjain@Shashwats-MacBook-Pro.local>
2019-09-19 09:27:59 +0800

    Hyperparam Adjust

--- src/scripts/create_trainer.sh
+++ src/scripts/create_trainer.sh
@@ -6,7 +6,7 @@ else
 fi
 # aws s3 sync ../datasets s3://dkitdatasets && \
 # Create new ec2 machine
-docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 150 --amazonec2-instance-type p3.2xlarge dkit-trainer && \
+docker-machine create --driver amazonec2 --amazonec2-iam-instance-profile ec2-trainer --amazonec2-ami ami-060865e8b5914b4c4 --amazonec2-root-size 500 --amazonec2-instance-type p3.2xlarge dkit-trainer && \
 # Setup Dkit in the system
 docker-machine ssh dkit-trainer << EOF
 source activate tensorflow_p36 && pip install git+https://github.com/aleju/imgaug.git && \
